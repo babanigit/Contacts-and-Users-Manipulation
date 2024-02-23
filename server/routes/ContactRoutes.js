649fae6ff0717a742c66d8dce608612d9546b3bb
bablu <aniketpanchal07@gmail.com>
2024-02-24 01:34:09 +0530

    new commit

--- server/routes/ContactRoutes.js
+++ server/routes/ContactRoutes.js
@@ -2,8 +2,26 @@ const express = require("express");
 
 const router = express.Router();
 
-router.route("/").get( (req, res) => {
-    res.status(200).json({ message: "get all contacts " });
+// router.route("/").get((req, res) => {
+//   res.status(200).json({ message: "get all contacts " });
+// });
+
+
+
+router.route("/").post((req, res) => {
+  res.status(200).json({ message: "create contacts " })
+});
+
+router.route("/:id").get((req, res) => {
+    res.status(200).json({ message: `get contact from ${req.params.id}  `});
   });
 
-  module.exports = router;
+router.route("/:id").put((req, res) => {
+  res.status(200).json({ message: `update contact from ${req.params.id}  `});
+});
+
+router.route("/:id").delete((req, res) => {
+  res.status(200).json({ message: `delete contacts from ${req.params.id} ` });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,41 @@
 const express = require("express");
 const app = express();
+app.use(express.json()); // this will enable us to use 'json' formatted data
+const dotenv = require("dotenv").config({ path: "./config.env" }); // just use it one time in index.js and no need to initialize it again and again
 
-const dotenv = require("dotenv").config();
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use("/api/contacts", require("./routes/ContactRoutes"));
 
 
 app.get("/", (req, res) => {
   res.send(`Hello World! on port ${port} `);
+   
 });
+
+
+// get request from postman and return response to postman 
+app.post("/trail",   async(req,res)=> {
+
+  try {
+    const { name , email } = await req.body;
+    res.json({message:`response from trail route ${name} and ${email} `})
+    // res.send(`${name} and ${email}  `)
+    console.log("hello")
+    // console.log(req.body)
+    
+  } catch (error) {
+    console.error(error)
+  }
+ 
+} );
+
+
+// app.use(express.json());
+// app.post("/trail",async (req,res)=> {
+//   const {name , email} = await req.body;
+//   res.json({message:`response from trail route ${name} and ${email} `})
+// })
 
 app.listen(port, () => {
   console.log(`server started on port ${port} `);
